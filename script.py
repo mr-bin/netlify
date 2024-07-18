bda3b75bcb50fdd12dc036c900a02525acc80f7a
mr_bin <zaikin.sergey@gmail.com>
2024-07-19 00:09:35 +0100

    unfinished site #1

--- script.py
+++ script.py
@@ -2,7 +2,7 @@ from jinja2 import Environment, FileSystemLoader
 
 environment = Environment(loader=FileSystemLoader("."))
 template = environment.get_template('index.html.template')
-content = template.render(title="Bootstrap demo")
+content = template.render(title="Сайт психолога")
 
 with open("public/index.html", mode="w", encoding="utf-8") as message:
     message.write(content)
