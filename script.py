172b60296096a0cb640c10c56cd21802921077e9
mr_bin <zaikin.sergey@gmail.com>
2024-11-03 17:00:57 +0000

    yaml

--- script.py
+++ script.py
@@ -1,8 +1,12 @@
+import yaml
+from yaml.loader import SafeLoader
 from jinja2 import Environment, FileSystemLoader
 
 environment = Environment(loader=FileSystemLoader("."))
+settings = yaml.load(open("settings.yaml"), Loader=SafeLoader)
 template = environment.get_template('index.html.template')
-content = template.render(title="Сайт психолога")
+content = template.render(title=settings["title"],
+                          price=settings["price"])
 
 with open("public/index.html", mode="w", encoding="utf-8") as message:
     message.write(content)
